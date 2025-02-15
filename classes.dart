ced9a6fe6b97099e7dcec7c079426a1425d33f76
Yunish <yunishban123@gmail.com>
2025-02-15 13:55:11 +0545

    classes added

--- classes.dart
+++ classes.dart
@@ -1,20 +1,21 @@
 void main(){
   final Yunish = College(SymbolNo: 23,name:  'Yurika');
   print(Yunish.returnConcatination());
+  Yunish.setName = 'yunish';
 }
 
 class College{
   // variables
   final int SymbolNo;
   final String name;
-  College({ required this.SymbolNo,required this.name}){
+  College({required this.SymbolNo,required this.name}){
      Topper();
   }
   // methods or functions
   void Topper(){
     print("The topper of 2025 is $name and her symbol no is $SymbolNo");
   }
-  // private variables
+  // private variables they are accessible only within the same file
   String _orihime = "taskete";
   String _ichigo = "kudasai";
 
@@ -22,6 +23,17 @@ class College{
   String returnConcatination(){
         return _orihime + _ichigo;
   }
-  
-  
+  void modifyOrihime(String a){
+    _orihime = a;
+  }
+  // getters
+  // String get orihime => _orihime;
+  // another way of doing the same thing
+  String get orihime{
+    return _orihime;
+  }
+  // setters
+  set setName(String ichigo){
+    _ichigo = ichigo;
+  }
 }
